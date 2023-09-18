a5d732318b7270026e417b714e5225043c3d458b
Richard <okaforichard2k17@gmail.com>
2023-09-18 18:02:24 +0100

    Second commit

--- app/src/main/java/com/example/affirmationsviews/MainActivity.kt
+++ app/src/main/java/com/example/affirmationsviews/MainActivity.kt
@@ -19,5 +19,8 @@ class MainActivity : AppCompatActivity() {
         // Use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true)
+
+        print("second commit")
     }
+
 }
