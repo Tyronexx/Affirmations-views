ce3c32b605455ba069b126d1ecad9defc7137342
Richard <okaforichard2k17@gmail.com>
2023-09-19 03:44:01 +0100

    Fifth commit

--- app/src/main/java/com/example/affirmationsviews/MainActivity.kt
+++ app/src/main/java/com/example/affirmationsviews/MainActivity.kt
@@ -21,6 +21,9 @@ class MainActivity : AppCompatActivity() {
         recyclerView.setHasFixedSize(true)
 
         print("second commit")
+        print("Local change")
+
+        print("Experimental branch")
     }
 
 }
